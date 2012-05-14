6195ab7665341493eeacbced32e30dddc85869c7
Thomas Hunter <tlhunter@gmail.com>
2012-05-14 00:59:43 -0400

    Different message display format, graceful ctrl+c shutdowns, handling common iwlist errors

--- app.js
+++ app.js
@@ -8,40 +8,40 @@ wireless.configure({
         stat: 'sudo ifconfig :INTERFACE',
     },
     iface: 'wlan0',
-    updateFrequency: 5,
+    updateFrequency: 8,
 });
 
 wireless.on('appear', function(error, network) {
     if (error) {
-        console.log("There was an error when a network appeared");
+        console.log("[   ERROR] There was an error when a network appeared");
         throw error;
     }
     var strength = Math.floor(network.quality / 70 * 100);
-    console.log("New: " + network.ssid + " [" + network.address + "] " + strength + "%");
+    console.log("[  APPEAR] " + network.ssid + " [" + network.address + "] " + strength + "%");
 });
 
 wireless.on('disappear', function(error, network) {
     if (error) {
-        console.log("There was an error when a network disappeared");
+        console.log("[   ERROR] There was an error when a network disappeared");
         throw error;
     }
-    console.log("Bye Network: " + network.ssid);
+    console.log("[    HIDE] Network: " + network.ssid);
 });
 
 wireless.on('change', function(error, network) {
     if (error) {
-        console.log("There was an error when a network changed");
+        console.log("[   ERROR] There was an error when a network changed");
         throw error;
     }
-    console.log("Network Changed: " + network.ssid);
+    console.log("[  CHANGE] " + network.ssid);
 });
 
 wireless.on('connect', function(error, network) {
-    console.log("Connected to: " + network.ssid);
+    console.log("[ CONNECT] " + network.ssid);
 });
 
 wireless.on('disconnect', function(error, network) {
-    console.log("Disconnected from Network: " + network.ssid);
+    console.log("[   DISCO] " + network.ssid);
     console.log("Don't be sad. There are still " + wireless.networks.length + " fish in the sea.");
 });
 
@@ -49,9 +49,17 @@ wireless.start(function() {
     var ssid = wireless.list()[5];
     wireless.join(ssid, null, function(error, network) {
         if (error) {
-            console.log("There was an error connecting to the fifth network");
+            console.log("[   ERROR] There was an error connecting to the fifth network");
             throw error;
         }
-        console.log("I've connected to the fifth network!");
+        console.log("[    INFO] I've connected to the fifth network!");
+    });
+});
+
+process.on('SIGINT', function() {
+    console.log("\nGracefully shutting down from SIGINT (Ctrl+C)");
+    // some other closing procedures go here
+    wireless.stop(function() {
+        process.exit();
     });
 });
